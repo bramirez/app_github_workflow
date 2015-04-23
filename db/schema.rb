83b2e6ad4be3e1052f33fe3fe09a42c925f30201
Braymon <brayramirez@Bray-MacBook-Pro.local>
2015-04-23 15:30:06 +0800

    add address to user

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150423072652) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
